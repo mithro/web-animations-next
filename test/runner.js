6f0c6f227a09a517e588b2ac07f37246848cf785
Tim 'mithro' Ansell <mithro@mithis.com>
2016-05-11 17:22:48 +1000

    Allow mocha to have failed tests.

--- test/runner.js
+++ test/runner.js
@@ -29,7 +29,28 @@ function loadWebAnimationsBuildTarget(target) {
       });
     }
 
-    mocha.run();
+    var runner = mocha.run();
+
+    runner.on('end', function(){
+      window.mochaResults = runner.stats;
+      window.mochaResults.reports = failedTests;
+    });
+
+    var failedTests = [];
+    function logFailure(test, err){
+      var flattenTitles = function(test){
+        var titles = [];
+        while (test.parent.title){
+          titles.push(test.parent.title);
+          test = test.parent;
+        }
+        return titles.reverse();
+      };
+
+      failedTests.push({name: test.title, result: false, message: err.message, stack: err.stack, titles: flattenTitles(test) });
+    };
+    runner.on('fail', logFailure);
+
   });
 
 })();
